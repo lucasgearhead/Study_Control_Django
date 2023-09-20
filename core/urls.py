8c0a33ba845b97fe29544260323b4e712ba6430c
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-09-20 20:38:55 -0300

    fix(all): deleted migrate

--- core/urls.py
+++ core/urls.py
@@ -5,15 +5,10 @@ from studyControl.views.studentView import StudentDetail, StudentList
 from studyControl.views.taskView import TaskDetail,TaskList
 
 urlpatterns = [
-    # URLs para Aluno
     path('students', StudentList.as_view(), name='student-list'),
     path('students/<int:pk>/', StudentDetail.as_view(), name='student-detail'),
-
-    # URLs para Disciplina
     path('disciplines/', DisciplineList.as_view(), name='discipline-list'),
     path('disciplines/<int:pk>/', DisciplineDetail.as_view(), name='discipline-detail'),
-
-    # URLs para Tarefa
     path('tasks/', TaskList.as_view(), name='task-list'),
     path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
 ]
