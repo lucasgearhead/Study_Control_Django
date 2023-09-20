f9fb4fbf8f1c1c477731d4418ee7fce09e7c2e2c
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-09-20 20:12:49 -0300

    feat(views): Views Created

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,11 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from ..studyControl.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/alunos/', AlunoListCreateView.as_view(), name='aluno-list-create'),
+    path('api/alunos/<int:pk>/', AlunoDetailView.as_view(), name='aluno-detail'),
+    path('api/disciplinas/', DisciplinaListCreateView.as_view(), name='disciplina-list-create'),
+    path('api/disciplinas/<int:pk>/', DisciplinaDetailView.as_view(), name='disciplina-detail'),
+    path('api/tarefas/', TarefaListCreateView.as_view(), name='tarefa-list-create'),
+    path('api/tarefas/<int:pk>/', TarefaDetailView.as_view(), name='tarefa-detail'),
 ]
--- studyControl/views/studentView.py
+++ studyControl/views/studentView.py
@@ -1,12 +1,44 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from ..models.studentModel import StudentModel
-from ..serializers.studentSerializer import StudentModelSerializer
+from ..serializers.studentSerializer import StudentSerializer
 
+class StudentList(APIView):
+    def get(self, request):
+        student = StudentModel.objects.all()
+        serializer = StudentSerializer(student, many=True)
+        return Response(serializer.data)
 
-class StudentModelList(generics.ListCreateAPIView):
-    queryset = StudentModel.objects.all()
-    serializer_class = StudentModelSerializer
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class StudentModelDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = StudentModel.objects.all()
-    serializer_class = StudentModelSerializer
+class StudentDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return StudentModel.objects.get(pk=pk)
+        except StudentModel.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        student = self.get_object(pk)
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        student = self.get_object(pk)
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
